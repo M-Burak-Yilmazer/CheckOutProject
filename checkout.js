de06c98341b0c6d41d5a2a14f6b879f87fb0e55d
mathiasburger <mburakyilmazer38@gmail.com>
2023-11-06 08:07:41 +0300

    cart-total

--- checkout.js
+++ checkout.js
@@ -21,14 +21,35 @@ document.querySelector(".nav").addEventListener("click", (e) => {
   }
 });
 
+let calculatePrices = () => {
+  const sumPrice = document.querySelector(".main__sum-price");
+  sumPrice.textContent = [
+    ...document.querySelectorAll(".main__product-line-price"),
+  ]
+    .reduce((acc, int) => acc + Number(int.textContent), 0)
+    .toFixed(2);
+
+  const taxPrice = (sumPrice.textContent * 0.18).toFixed(2);
+  document.querySelector("div#cart-tax .dollar").textContent = taxPrice;
+  console.log(taxPrice);
+
+  const shippingPrice = sumPrice.textContent > 3000 ? "0.00" : "25.99";
+  document.querySelector("div#cart-shipping .dollar").textContent = shippingPrice;
+
+  const totalPrice= (+(sumPrice.textContent) +  +(taxPrice)+ +(shippingPrice)).toFixed(2)
+  document.querySelector("div#cart-total ")
+  console.log(totalPrice)
+
+  return sumPrice.textContent;
+};
+console.log(document.querySelector("div#cart-tax .dollar").textContent);
+calculatePrices()
+
 //todo:   MAIN PART
 
 document
   .querySelector(".main__products-preview")
   .addEventListener("click", (e) => {
-
-
-
     //* +,-, silme tuşlarına basıldığında
     if (e.target.classList.contains("fa-plus")) {
       e.target.previousElementSibling.textContent++;
@@ -39,6 +60,7 @@ document
           .closest(".main__product-info")
           .querySelector(".main__product-price--text .dollar").textContent
       ).toFixed(2);
+      calculatePrices();
     } else if (e.target.classList.contains("fa-minus")) {
       if (e.target.nextElementSibling.textContent >= 1) {
         e.target.nextElementSibling.textContent--;
@@ -49,6 +71,7 @@ document
               .closest(".main__product-info")
               .querySelector(".main__product-price--text .dollar").textContent
           ).toFixed(2);
+        calculatePrices();
       }
       if (e.target.nextElementSibling.textContent < 1) {
         let result = confirm(
@@ -63,7 +86,7 @@ document
           : (e.target.nextElementSibling.textContent = 1);
       }
     }
-    //* silme tuşuna basıldığında 
+    //* silme tuşuna basıldığında
     else if (e.target.classList.contains("fa-trash-can")) {
       let result = confirm(
         `${
@@ -73,5 +96,6 @@ document
         } will be removed!`
       );
       result ? e.target.closest(".main__product").remove() : null;
+      calculatePrices();
     }
   });
